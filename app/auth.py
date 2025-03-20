18b30cab1ac74b8fef79d2998f7433403da896a5
sergkstudio <sergkstudio@gmail.com>
2025-03-20 21:56:05 +0300

    1

--- app/auth.py
+++ app/auth.py
@@ -1,18 +1,20 @@
-from flask import request, jsonify
+from flask import request, jsonify, render_template
 from app import app
 from ldap3 import Server, Connection, ALL
 import os
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    username = request.json.get('username')
-    password = request.json.get('password')
-    domain = os.getenv('DOMAIN')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        domain = os.getenv('DOMAIN')
 
-    server = Server(domain, get_info=ALL)
-    conn = Connection(server, user=f'{domain}\\{username}', password=password, authentication='NTLM')
+        server = Server(domain, get_info=ALL)
+        conn = Connection(server, user=f'{domain}\\{username}', password=password, authentication='NTLM')
 
-    if conn.bind():
-        return jsonify({'message': 'Login successful'}), 200
-    else:
-        return jsonify({'message': 'Invalid credentials'}), 401
+        if conn.bind():
+            return jsonify({'message': 'Login successful'}), 200
+        else:
+            return jsonify({'message': 'Invalid credentials'}), 401
+    return render_template('login.html')
