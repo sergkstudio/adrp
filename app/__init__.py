22687fe79c09b2d7b0dec0cadfd1c8daa552c978
sergkstudio <sergkstudio@gmail.com>
2025-03-20 21:12:26 +0300

    1

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_envvar('APP_SETTINGS')
+    # Удалите или закомментируйте следующую строку, если не используете файл конфигурации
+    # app.config.from_envvar('APP_SETTINGS')
 
     with app.app_context():
         from . import routes
